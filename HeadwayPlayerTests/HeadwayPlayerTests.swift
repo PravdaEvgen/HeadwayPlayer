6168da6f7a96c5da9e9d694b7f6517b9647dce2a
Yevhen Pravda <pravdaievgena@gmail.com>
2024-11-20 04:38:42 +0200

    add Tests for reducer

--- HeadwayPlayerTests/HeadwayPlayerTests.swift
+++ HeadwayPlayerTests/HeadwayPlayerTests.swift
@@ -2,16 +2,150 @@
 //  HeadwayPlayerTests.swift
 //  HeadwayPlayerTests
 //
-//  Created by Yevhen Pravda on 18.11.2024.
+//  Created by Yevhen Pravda on 19.11.2024.
 //
 
+import ComposableArchitecture
 import Testing
 @testable import HeadwayPlayer
 
+@MainActor
 struct HeadwayPlayerTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    
+    @Test
+    func bookSummaryFetchOnStart() async {
+        let clock = TestClock()
+        let localProvider = LocalBookSummaryResources()
+        let store = TestStore(initialState: BookSummaryFeature.State()) {
+            BookSummaryFeature()
+        } withDependencies: {
+            $0.continuousClock = clock
+        }
+        store.exhaustivity = .off
+        
+        await store.send(.onAppear)
+        await store.receive(\.updateBookSummary) {
+            $0.bookSummary = .init(coverURL: localProvider.coverURL, chapters: localProvider.chapters)
+        }
+    }
+    
+    @Test
+    func bookSummaryFetchOnStartEmpty() async {
+        let clock = TestClock()
+        let store = TestStore(initialState: BookSummaryFeature.State()) {
+            BookSummaryFeature()
+        } withDependencies: {
+            $0.continuousClock = clock
+        }
+        store.exhaustivity = .off
+        
+        await store.send(.onAppear)
+        await store.send(.updateBookSummary(nil)) {
+            $0.bookSummary = nil
+        }
     }
 
+    @Test
+    func errorAlertPresentedAndDismissed() async {
+        let store = TestStore(initialState: BookSummaryFeature.State()) {
+            BookSummaryFeature()
+        }
+        store.exhaustivity = .off
+        
+        await store.send(.error("Test error message")) {
+            $0.errorAlert = AlertState {
+                TextState("Error during playback occured")
+            } actions: {
+                ButtonState(role: .cancel) {
+                  TextState("Got it!")
+                }
+              } message: {
+                TextState("Test error message")
+              }
+        }
+        await store.send(.errorAlert(.dismiss)) {
+            $0.errorAlert = nil
+        }
+    }
+    
+    @Test
+    func playingNextIsChangingIndex() async {
+        let clock = TestClock()
+        let store = TestStore(initialState: BookSummaryFeature.State()) {
+            BookSummaryFeature()
+        } withDependencies: {
+            $0.continuousClock = clock
+        }
+        store.exhaustivity = .off
+        
+        await store.send(.onAppear)
+        await store.send(.playChapter(0))
+        await store.send(.playNextChapter) {
+            $0.currentIndex = 0
+        }
+        await store.send(.playNextChapter) {
+            $0.currentIndex = 1
+        }
+        await store.send(.playNextChapter) {
+            $0.currentIndex = 2
+        }
+        await store.send(.playNextChapter) {
+            $0.currentIndex = 3
+        }
+        await store.send(.playNextChapter) {
+            $0.currentIndex = 3
+        }
+    }
+    
+    @Test
+    func playingPreviousIsChangingIndex() async {
+        let clock = TestClock()
+        let store = TestStore(initialState: BookSummaryFeature.State()) {
+            BookSummaryFeature()
+        } withDependencies: {
+            $0.continuousClock = clock
+        }
+        store.exhaustivity = .off
+        
+        await store.send(.onAppear)
+        await store.send(.playChapter(3))
+        await store.send(.playPreviousChapter) {
+            $0.currentIndex = 3
+        }
+        await store.send(.playPreviousChapter) {
+            $0.currentIndex = 2
+        }
+        await store.send(.playPreviousChapter) {
+            $0.currentIndex = 1
+        }
+        await store.send(.playPreviousChapter) {
+            $0.currentIndex = 0
+        }
+        await store.send(.playPreviousChapter) {
+            $0.currentIndex = 0
+        }
+    }
+    
+    @Test
+    func pauseIsPausingPlayIsResuming() async {
+        let clock = TestClock()
+        let store = TestStore(initialState: BookSummaryFeature.State()) {
+            BookSummaryFeature()
+        } withDependencies: {
+            $0.continuousClock = clock
+        }
+        store.exhaustivity = .off
+        
+        await store.send(.onAppear)
+        await store.send(.playChapter(0)) {
+            $0.isPlaying = true
+        }
+        await clock.advance(by: .seconds(2))
+        await store.send(.pause) {
+            $0.isPlaying = false
+        }
+        await store.send(.resume) {
+            $0.isPlaying = true
+        }
+    }
 }
